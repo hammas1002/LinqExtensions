98f9ffe12d429b1a413d823978de3756585c3f0a
Hamma <hammas1002@gmail.com>
2022-05-20 15:14:10 +0500

    Add project files.

---
+++ Linq/LinqExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace System.Linq2
+{
+    static class LinqExtensions
+    {
+        public static IEnumerable<T> Where2<T>(this IEnumerable<T> e,Predicate<T> predicate)
+        {
+            List<T> tempList=new();
+            foreach(var item in e)
+            {
+                if (predicate(item))
+                {
+                    tempList.Add(item);
+                }
+            }
+            return tempList;
+            
+        }
+        public static double Max2(this IEnumerable<double> e)
+        {
+            var myList = e.ToList();
+            if (myList == null) { throw new InvalidOperationException(); }
+            double maxNum = myList[0];
+            foreach (var item in e)
+            {
+                if (maxNum < item) maxNum = item;
+            }
+            return maxNum;
+        }
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq2;
+using System.Linq;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> myList = new() { 1.2, 5.4, 9.0, 2.7, 6.6 };
+            List<int> myListInt = new() { 1,5,8,12,3,6 };
+            List<int> EmptyList = new();
+            /*List<double> myShortList = new();*/
+            /*var myShortList = myList.Where2(num => num < 6.5).Where2(num => num > 5);*/
+            /*foreach (var a in myShortList)
+            {
+                Console.WriteLine(a);
+            }*/
+            var myShortList = myList.Where2(num => num < 6.5);
+            Console.WriteLine(@"
+---------------------
+");
+            foreach (var a in myShortList)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine(@"
+---------------------
+");
+                
+        }
+    }
+}
